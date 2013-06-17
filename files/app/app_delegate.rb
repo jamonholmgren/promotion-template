2d52092ae42384409f22dba23eb3b7a41211f9b6
Gant <GantMan@gmail.com>
2013-06-17 15:04:52 -0500

    initial commit of ProMotion template

---
+++ files/app/app_delegate.rb
@@ -0,0 +1,21 @@
+class AppDelegate < PM::Delegate
+  
+  status_bar false, animation: :none
+
+  def on_load(app, options)
+    register_for_push_notifications :all
+    
+    open_tab_bar HomeScreen, StatesScreen, ContactScreen, HelpScreen.new(nav_bar: true)
+  end
+  
+  # Push Notifications (PM version 0.7+)
+  def on_push_notification(notification)
+    PM.logger.info notification
+  end
+  
+  def on_push_registration(token, error)
+    # Push up token to your server here
+
+  end
+
+end
