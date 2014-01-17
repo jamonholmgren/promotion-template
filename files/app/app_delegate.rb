89cc52d8fdc1a5cb66ac7357bd9a4d7824e95ccb
Jamon Holmgren <jamon@clearsightstudio.com>
2014-01-16 21:05:17 -0800

    ProMotion 1.1.2, added log level setting

--- files/app/app_delegate.rb
+++ files/app/app_delegate.rb
@@ -2,6 +2,7 @@ class AppDelegate < PM::Delegate
   status_bar true, animation: :none
 
   def on_load(app, options)
+    PM.logger.level = :verbose # :off, :error, :warn, :info, :debug, :verbose
     open HomeScreen.new(nav_bar: true)
   end
   
