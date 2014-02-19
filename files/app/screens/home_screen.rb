a76af55c5d5c1054caea5afbaecd09f6723eb906
Jamon Holmgren <jamon@clearsightstudio.com>
2014-02-18 18:28:02 -0800

    Removed boilerplate

--- files/app/screens/home_screen.rb
+++ files/app/screens/home_screen.rb
@@ -1,29 +1,10 @@
 class HomeScreen < PM::Screen
-  include HomeStyles
-
   title "Home"
 
   def on_load
-    set_nav_bar_button :left, title: "Help", action: :help_tapped
-    set_nav_bar_button :right, title: "States", action: :states_tapped
   end
 
   def will_appear
-    @view_setup ||= self.set_up_view
   end
 
-  def set_up_view
-    set_attributes self.view, :home_view_style # found in HomeStyles module
-    add UILabel.new, :label_view_style
-
-    true
-  end
-
-  def states_tapped
-    open StatesScreen
-  end
-
-  def help_tapped
-    open_modal HelpScreen.new(nav_bar: true)
-  end
 end
