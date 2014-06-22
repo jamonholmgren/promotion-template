73545b07adf31c5b6eb08f59249f532932e33cb3
Jamon Holmgren <jamon@clearsightstudio.com>
2014-06-21 18:34:07 -0700

    Updated for ProMotion 2.0

--- files/app/app_delegate.rb
+++ files/app/app_delegate.rb
@@ -3,7 +3,8 @@ class AppDelegate < PM::Delegate
   status_bar true, animation: :none
 
   def on_load(app, options)
+    return true if RUBYMOTION_ENV == "test"
     open HomeScreen.new(nav_bar: true)
   end
-  
+
 end
---
+++ files/spec/screens/home_screen_spec.rb
@@ -0,0 +1,28 @@
+describe HomeScreen do
+  tests HomeScreen
+
+  def home_screen
+    @home_screen ||= HomeScreen.new(nav_bar: true)
+  end
+
+  def controller
+    home_screen.navigationController
+  end
+
+  after { @home_screen = nil }
+
+  it "has a navigationController" do
+    home_screen.navigationController.should.be.kind_of(UINavigationController)
+  end
+
+  it "has a right nav bar button" do
+    home_screen.navigationItem.rightBarButtonItem.should.be.instance_of UIBarButtonItem
+  end
+
+  it "opens the help screen when tapping Help" do
+    tap("Help")
+    controller.topViewController.should.be.kind_of(HelpScreen)
+  end
+
+end
+
